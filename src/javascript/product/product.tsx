f300b3ba23505aa8d30003bc0f3ee2ac52ec6757
Matt Mannion <mattpmannion@gmail.com>
2021-01-27 23:10:37 -0500

    optimization testing

--- src/javascript/product/product.tsx
+++ src/javascript/product/product.tsx
@@ -1,12 +1,11 @@
 import { VFC } from 'react';
-import loadable from '@loadable/component';
 import logo from '../../resources/icons/ms-icon/ms-icon-70x70.png';
 import viewerItem from '../../resources/images/vendor/unsplash/strat-item.jpg';
 import mainItem from '../../resources/images/vendor/unsplash/strat-main.jpg';
 
 // components
-const Viewer = loadable(() => import('./product-comps/viewer'));
-const Details = loadable(() => import('./product-comps/details'));
+import Viewer from './product-comps/viewer';
+import Details from './product-comps/details';
 import Description from './product-comps/description';
 
 const Product: VFC = () => (
